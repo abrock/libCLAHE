0caaf7bc3e37e99535f1acea9f07fc3db01e9ed3
Alexander Brock <alexander@lunar-orbit.de>
2017-03-16 17:26:30 +0100

    Backup

--- clahe.cpp
+++ clahe.cpp
@@ -3,9 +3,44 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
+std::string type2str(int type) {
+  std::string r;
+
+  uchar depth = type & CV_MAT_DEPTH_MASK;
+  uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+  switch ( depth ) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+  }
+
+  r += "C";
+  r += (chans+'0');
+
+  return r;
+}
+
 cv::Mat applyCLAHE(const cv::Mat& _bgr_image, double const clipLimit, int const tilesGridSize) {
     cv::Mat bgr_image;
     cv::normalize(_bgr_image, bgr_image, 0, 1, cv::NORM_MINMAX, CV_32FC3);
+
+    if (bgr_image.channels() == 1) {
+        cv::Mat tmp;
+        cv::cvtColor(bgr_image, tmp, CV_GRAY2BGR, 3);
+        bgr_image = tmp;
+    }
+
+    if (bgr_image.channels() != 3 && bgr_image.channels() != 4) {
+        std::cout << "Unexpected number of channesl (" << bgr_image.channels() << "), expected 3 or 4" << std::endl;
+        std::cout << "Original image type: " << type2str(_bgr_image.type()) << std::endl;
+        std::cout << "Converted image type: " << type2str(bgr_image.type()) << std::endl;
+    }
 
     cv::Mat lab_image;
     cv::cvtColor(bgr_image, lab_image, CV_BGR2Lab);
